9e6b69b60c36ec32133e7e7a1c77576971715628
davidonasanya12 <119113107+davidonasanya12@users.noreply.github.com>
2024-05-26 13:30:50 +0100

    Add files via upload

---
+++ Roll a dice - PRO_DAVID_101.py
@@ -0,0 +1,52 @@
+import random
+input("Do you want to roll the dice: ")
+response = "y"
+while response == "y":
+    no = random.randint(1, 6)
+    if no == 1:
+        print("[-----]")
+        print("[     ]")
+        print("[  0  ]")
+        print("[     ]")
+        print("[-----]")
+    elif no == 2:
+        print("[-----]")
+        print("[ 0   ]")
+        print("[     ]")
+        print("[   0 ]")
+        print("[-----]")
+    elif no == 3:
+        print("[-----]")
+        print("[  0  ]")
+        print("[  0  ]")
+        print("[  0  ]")
+        print("[-----]")
+    elif no == 4:
+        print("[-----]")
+        print("[ 0 0 ]")
+        print("[     ]")
+        print("[ 0 0 ]")
+        print("[-----]")
+    elif no == 5:
+        print("[-----]")
+        print("[ 0 0 ]")
+        print("[  0  ]")
+        print("[ 0 0 ]")
+        print("[-----]")
+    elif no == 6:
+        print("[-----]")
+        print("[ 0 0 ]")
+        print("[ 0 0 ]")
+        print("[ 0 0 ]")
+        print("[-----]")
+
+    response = input("Do you want to roll the dice again? (y/n): ")  
+
+     
+
+   
+        
+
+    
+
+        
